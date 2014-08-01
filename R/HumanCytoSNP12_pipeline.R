d57e3ec5df06cecd9cc0a1db4ffc4513b3e7b8fc
Daniel Silvestre <daniel.silvestre@hc.fm.usp.br>
2014-08-01 19:01:06 -0300

    Refactor the basic pipeline
    The HumanCytoSNP12 now has parallel calculations for SMP machines. The
    principal downstream analysis loop produces:
     - Reduced ranges per copy state;
     - Ranges with coverage score per state;
    
     Both calculations print data files for Circos/sheet stuff (tsv files).
     Sample names present only on tsv file. Ranges with coverage are intented to be
     used as tiles 2D track data (or similar). Reduced ranges as highlights data
     track.

--- R/HumanCytoSNP12_pipeline.R
+++ R/HumanCytoSNP12_pipeline.R
@@ -1,6 +1,10 @@
 # load configuration stuff
 source('conf/chromothripsis.rconf')
 
+# Let's parallelize calculations.
+registerDoMC(12)
+ocSamples(12)
+
 # Genotyping
 gen <- genotype.Illumina(sampleSheet=sampleSheet, arrayNames=arrayNames, arrayInfoCol=arrayInfo, cdf=cdfName, batch=batch, sns=sns, col.names=TRUE)
 
@@ -27,114 +31,71 @@ crlmmCopynumber(gen)
 # Will generate some warnings
 bls <- BafLrrSetList(gen)
 
-# Illumina generates data outside chromosome range
+# Illumina generates data outside normal chromosome range
 # Not sure what it means. Should I ask on foruns?
 # Functions will not work on this strange data
 # Let's filter it out for now
 
-cleanedBls <- bls[-(1:1)] 
+cleaned_bls <- bls[-(1:1)] 
 
 # Correct for genomic waves
 # As this is a BafLrrSetList, return value is NULL
 # Everything is done in place
 # How do I calibrate the window size?
-windowSize <- 10^(2:6)
+
+window_size <- 10^(2:6)
 increments <- 2 * 10^(1:5)
-ArrayTV:::gcCorrect(cleanedBls, maxwins=windowSize, increms=increments)
+ArrayTV:::gcCorrect(cleaned_bls, maxwins=window_size, increms=increments)
 
 # Fit HMM to identify CNVs
-fit <- hmmBafLrrSetList2(cleanedBls)
+fit <- unlist(hmmBafLrrSetList2(cleaned_bls))
+
+# Back to serial mode.
+stopCluster(cluster)
 
 # Downstream analysis
-ranges <- unlist(fit)
-
-# Need to adjust chromosome names
-chrs <- paste('chr', chromosome(bls), sep='')
-filtered_bls <- bls[chrs %in% chromosome(ranges)]
-# Filter those chromosomes that generated ranges
-IDs <- unique(sampleNames(ranges))
-
-#Find overlapping ranges by state
+# Idea is to reduce ranges to a single data track and add a coverage track
 
 for(st in 1:6){
 
-	filtered_ranges <- ranges[state(ranges) == st, ]
-	tiles <- paste('all.overlapping.', st, '.ranges.dat', sep='')
-	tsv <- paste('all.overlapping.', st, '.ranges.tsv', sep='')
-	hist <- paste('all.overlapping.', st, '.ranges.hist', sep='')
-	
-	tmp <- as.data.frame(filtered_ranges, row.names=NULL)
-	tmp$group <- subjectHits(findOverlaps(filtered_ranges, reduce(filtered_ranges)))
-	tmp <- tmp[which(tmp$width > 1), ]
-	tmp <- as.data.table(tmp)
-	tmp <- tmp[, list(start=min(start), end=max(end), samples=list(unique(sample))), by=list(group, seqnames)]
+    ranges <- fit[state(fit) == st, ]
+    ranges_with_coverage <- as(coverage(ranges), 'GRanges')
+    reduced_ranges <- reduce(ranges)
+    overlaps <- findOverlaps(ranges, ranges_with_coverage)
+    qidx <- queryHits(overlaps)
+    sidx <- subjectHits(overlaps)
+    tmp <- tapply(sampleNames(ranges)[qidx], sidx, list)
+    ranges_with_coverage$samples <- NA
+    ranges_with_coverage$samples[as.integer(names(tmp))] <- tmp
+    ranges_with_coverage$samples <- sapply(ranges_with_coverage$samples,
+                                           FUN=paste, collapse='|')
 
-	tmp$counts <- sapply(tmp$samples, FUN=length)
-	tmp$samples <- sapply(tmp$samples, FUN=paste, collapse='|')
-	
-	data <- data.frame('CHR'=gsub('chr', 'hs', tmp$seqnames), 'START'=tmp$start, 'END'=tmp$end)
-	data$STATE <- paste('state=', rep(st, dim(data)[1]), sep='')
-	write.table(data, file=tiles, sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
+    # Print coverage data as tsv and to feed in Circos
+    df <- as.data.frame(ranges_with_coverage, row.names=NULL)
+    df <- as.data.table(df)
+    df$strand <- c()
+    df <- df[df$score > 0 || df$width == 1, ]
 
-	data <- data.frame('CHR'=gsub('chr', 'hs', tmp$seqnames), 'START'=tmp$start, 'END'=tmp$end, COUNTS=tmp$counts * 10)
-	write.table(data, file=hist, sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
+    # Table data/Coverage
+    write.table(df, file=paste0('ranges_with_coverage_', st, '.tsv'), sep='\t', row.names=FALSE, quote=FALSE)
 
+    # Circos/Coverage
+    data <- data.frame('CHR'=gsub('chr', 'hs', df$seqnames),
+                                'START'=df$start, 'END'=df$end)
+	data$SCORE <- paste0('score=', df$score)
+	write.table(data, file=paste0('ranges_with_coverage_for_circos_', st,
+                                 '.dat'), sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
 
-	write.table(tmp, file=tsv, sep='\t', row.names=FALSE, quote=FALSE)
+    # Circos/Reduced range
+	df <- as.data.frame(reduced_ranges, row.names=NULL)
+    df$strand <- c()
+    df <- df[df$width == 1, ]
 
-}
-
-for(ID in IDs){
-
-	# Filter by sample ID
-	filtered_ranges <- ranges[state(ranges) %in% c(1, 2, 5, 6) & sampleNames(ranges) == ID, ]
-	filename <- paste(ID, '.ranges.dat', sep='')
-	
-	tmp <- as.data.frame(filtered_ranges, row.names=NULL)
-
-	idx <- which(tmp$width > 1)	
-	CHR <- gsub('chr', 'hs', tmp$seqnames[idx])
-	START <- tmp$start[idx]
-	END <- tmp$end[idx]
-	STATE <- paste('state=', tmp$state[idx], sep='')
-	#COLOR <- paste('color=', c('red', 'orange', 'white', 'white', 'cyan', 'blue')[STATE], sep='')
-
-	
-	data <- data.frame('CHR'=CHR, 'START'=START, 'END'=END, 'STATE'=STATE)
-	#data <- data.frame('CHR'=CHR, 'START'=START, 'END'=END, 'STATE'=STATE, 'COLOR'=COLOR)
-	
-	write.table(data, file=filename, sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
+    data <- data.frame('CHR'=gsub('chr', 'hs', df$seqnames),
+                       'START'=df$start, 'END'=df$end)
+	write.table(data, file=paste0('reduced_ranges_for_circos_', st,
+                                 '.dat'), sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
 
 }
 
-for(ID in IDs){
-
-	# Filter by sample ID
-	filtered_ranges <- ranges[sampleNames(ranges) == ID, ]
-	tmp <- filtered_bls[ , match(ID, sampleNames(filtered_bls))]
-
-	dataLRR <- data.frame('CHR'=NA, 'START'=NA, 'END'=NA, 'LRR'=NA)
-	dataBAF <- data.frame('CHR'=NA, 'START'=NA, 'END'=NA, 'BAF'=NA)
-	for(i in 1:length(tmp)){
-
-		CHR <- unlist(paste('hs', chromosome(tmp[i]), sep=''))
-		START <- unlist(position(tmp[i]))
-		END <- unlist(position(tmp[i]))
-		LRR <- unlist(lrr(tmp[i]))/100
-		BAF <- unlist(baf(tmp[i]))/1000
-		#COLOR <- ifelse(LRR == 0, 'color=blue', 'color=red')
-		dflrr <- data.frame('CHR'=CHR, 'START'=START, 'END'=END, 'LRR'=LRR)
-		dfbaf <- data.frame('CHR'=CHR, 'START'=START, 'END'=END, 'BAF'=BAF)
-		dataLRR <- rbind(dataLRR, dflrr)
-		dataBAF <- rbind(dataBAF, dfbaf)
-		
-	}
-
-	filename <- paste(ID, '.LRR.dat', sep='')
-	write.table(dataLRR, file=filename, sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
-	
-	filename <- paste(ID, '.BAF.dat', sep='')
-	write.table(dataBAF, file=filename, sep=' ', row.names=FALSE, col.names=FALSE, quote=FALSE)
-
-}
 
