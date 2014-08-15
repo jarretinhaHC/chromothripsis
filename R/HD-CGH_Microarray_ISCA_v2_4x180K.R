b8e9cf7c4e09cfc3a9818950a533dc3938952ba2
Daniel Silvestre <daniel.silvestre@hc.fm.usp.br>
2014-08-15 09:52:03 -0300

    Pipeline to analyze Agilent SurePrint aCGH
    A complete pipeline to read post-scanning data, careful normalization, wave
    correction and genomic artifact mitigation based on:
    
        doi:10.1186/gb-2011-12-8-r80
    
    Certain doubts remain about the methodology. Authors should be inquired. After
    normalization, data is segmented and copy state called with aid of CGHcall
    package.

---
+++ R/HD-CGH_Microarray_ISCA_v2_4x180K.R
@@ -0,0 +1,155 @@
+# Remember to create apropriate conf files
+#source('rconf/agilent.rconf')
+
+library(doMC)
+library(foreach)
+library(cghMCR)
+library(Repitools)
+library(BSgenome.Hsapiens.UCSC.hg19)
+
+# Confs
+
+registerDoMC(cores=24)
+genome <- BSgenome.Hsapiens.UCSC.hg19
+
+basedir <- '/data/Cytogenomics/Agilent'
+datadir <- paste(basedir, 'raw/afe', sep='/')
+target_sheet <- 'targets.tsv'
+targets <- readTargets(paste(basedir, target_sheet, sep='/'), row.names='Name') 
+
+
+
+# Read target files and add some info
+# Agilent data is previously dye bias normalized with aCGH.Spline
+# Why the writeFE options create so badly behaved filenames?
+# batch.spline(dir=datadir, writeFE=TRUE)
+
+raw <- read.maimages(targets, path=datadir, source='agilent', names=targets$Name)
+raw$design <- rep(-1, dim(raw)[[2]])
+raw$ID <- targets$Name
+
+# Chromosome info and position are buried inside genes$SystematicName
+# Need to split the string and retrieve chr and start coord
+# Only from genes$ControlType == 0 (1 and -1 are true controls)
+
+chr_set <- paste0('chr', c(1:22, 'X', 'Y'))
+coords <- ifelse(!raw$genes$ControlType, strsplit(raw$genes$SystematicName, '[:-]'),
+                 NA)
+
+raw$genes$ID <- 1:dim(raw$genes)[[1]]
+raw$genes$chr <- sapply(coords, function(x) ifelse(x[1] %in% chr_set, x[1], NA))
+raw$genes$start <- sapply(coords, function(x) ifelse(x[1] %in% chr_set,
+                                                        as.numeric(x[2]), NA))
+raw$genes$end <- sapply(coords, function(x) ifelse(x[1] %in% chr_set,
+                                                        as.numeric(x[3]), NA))
+
+# Manual normalization based on Chen, J. et al. (2011)
+# See doi: 10.1186/gb-2011-12-8-r80 
+
+# We don't need controls and related stuff without coords
+tmp <- raw[which(raw$genes$chr %in% chr_set),]
+
+# Remove background based on median values, threshold as you like
+threshold <- 5
+median_bg_G <- apply(tmp$Gb[tmp$genes$chr %in% chr_set, , drop=FALSE], 2,
+                     median, na.rm=TRUE)
+median_bg_R <- apply(tmp$Rb[tmp$genes$chr %in% chr_set, , drop=FALSE], 2,
+                     median, na.rm=TRUE)
+
+bg_flag <- (tmp$G > threshold * median_bg_G) & (tmp$R > threshold *
+                                                median_bg_R)
+tmp$G[!bg_flag] <- NA
+tmp$R[!bg_flag] <- NA
+
+# Log intensities
+
+tmp$G <- log2(tmp$G)
+tmp$R <- log2(tmp$R)
+
+# Calculate trimmed means
+# Trim parameter is calculated with aid of quantiles/scale
+
+calculate_trim <- function(x){
+
+    observed <- scale(quantile(x, prob=seq(0, 1, 0.01)))
+    expected <- scale(quantile(rnorm(1000, mean(x), sd(x)), prob=seq(0, 1, 0.01)))
+    diff <- observed - expected
+    trim <- 0.01 * length(diff[which(abs(diff) > 1)])
+    return(trim)
+
+}
+
+trimG <- calculate_trim(tmp$G)
+trimR <- calculate_trim(tmp$R)
+
+# Trimmed means per sample
+tmp$Gtm <- apply(tmp$G, 2, mean, trim=trimG)
+tmp$Rtm <- apply(tmp$R, 2, mean, trim=trimR)
+
+# SD per sample
+tmp$Gsd <- apply(tmp$G, 2, sd)
+tmp$Rsd <- apply(tmp$R, 2, sd)
+
+for(sample in  tmp$ID){
+
+    tmp$G[, sample] <- (tmp$G[, sample] - tmp$Gtm[sample]) / tmp$Gsd[sample] 
+    tmp$R[, sample] <- (tmp$R[, sample] - tmp$Rtm[sample]) / tmp$Rsd[sample] 
+
+}
+
+# Normalized ratio and average intensities
+tmp$M <- tmp$G - tmp$R
+tmp$A <- (tmp$G + tmp$R) / 2
+
+# Genomic waves correction
+# Parameters suggested by previous studies with ArrayTV
+# Window size of about 120kbp for Agilent
+
+ranges <- sortSeqlevels(as(tmp$genes[, c('chr', 'start', 'end')], 'GRanges'))
+seqlengths(ranges) <- seqlengths(genome)[which(seqlevels(genome) %in% chr_set)]
+
+width <- 60e4
+seqs <- getSeq(genome, ranges, width=60e4)
+# GC content calculations can take a long time
+# Let's parallelize it!
+tmp$genes$GC <- foreach(i=1:length(seqs), .combine=c) %dopar%
+letterFrequency(seqs[i], letters='GC', as.prob=TRUE)
+
+# Set GC weights
+tmp$genes$w <- ifelse(tmp$genes$GC < 0.5, 1e-3, 1e3)
+
+# Fit model, just love R!
+# Residuals are the wave corrected M
+fit <- lm(tmp$M ~ tmp$genes$GC + I(tmp$genes$GC^2), weights=tmp$genes$w)
+tmp$Mwc <- fit$residuals
+
+# Now, correct artifacts
+# Remove medians per sample
+for(sample in tmp$ID) tmp$Mwc[, sample] <- tmp$Mwc[, sample] - median(tmp$Mwc[,
+                                                                      sample])
+# Probe means across samples for profiling
+tmp$genes$ProbeMeanMwc <- apply(tmp$Mwc, 1, mean)
+
+# Calculate profile 
+tmp$genes$ProbeProfile <- smooth.splines(tmp$genes$ProbeMeanMwc,
+                                         all.knots=TRUE)$y
+# Get the final M value!!!
+tmp$Mf <- tmp$Mwc - tmp$genes$ProbeProfile
+
+# Segmentation and copy state calling with CGHCall
+segs <- make_cghRaw(data.frame(tmp$genes$ProbeName, tmp$genes[, c('chr',
+                                                                 'start',
+                                                                 'end')],
+                              tmp$Mf, check.names=FALSE))
+
+# Data is already cleaned, so proceed to segmentation
+# Default parameters are fine
+segs <- segmentData(segs)
+segs <- postsegnormalized(segs)
+
+# It's show time! Call it!
+result <- CGHcall(segs)
+result <- ExpandCGHcall(result, segs)
+
+
+
